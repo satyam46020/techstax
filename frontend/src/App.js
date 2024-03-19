c5692054a35bb8997cd4239cdf3ae0a1b5a7eae1
satyam46020 <satyam46020@gmail.com>
2024-03-19 13:01:15 +0530

    basic structure

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import UploadData from './pages/UploadData';
+import WorkflowBuilder from './components/WorkflowBuilder';
+import WorkflowExecutor from './components/WorkflowExecutor';
+
+const App = () => {
+  return (
+    <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/upload" element={<UploadData/>} />
+        <Route path="/builder" element={<WorkflowBuilder/>} />
+        <Route path="/execution" element={<WorkflowExecutor/>} />
+     </Routes>
+  );
+};
+
+export default App;
